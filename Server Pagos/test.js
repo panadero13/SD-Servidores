1588b340633914056ed696aa981b998f8a12e44f
Luis Miguel Panadero <lmpc3@alu.ua.es>
2021-07-05 01:52:54 +0200

    Eliminado archivo inutil

--- Server Pagos/test.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require('ws');
-const ws = new WebSocket("ws://localhost:3005");
-
-ws.addEventListener('open', () => {
-    ws.send(JSON.stringify({
-        'user': 'Luis Miguel',
-        'card': 3334466
-    }));
-})
